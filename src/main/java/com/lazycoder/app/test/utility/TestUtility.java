a4e6627d4a44e4c05997d8ba65939029e4b1ea90
Punna <dharmasaiteja.p@gmail.com>
2017-08-15 23:03:06 -0500

    Updates to utility to read files in any Operating System

--- src/main/java/com/lazycoder/app/test/utility/TestUtility.java
+++ src/main/java/com/lazycoder/app/test/utility/TestUtility.java
@@ -1,47 +1,44 @@
 package com.lazycoder.app.test.utility;
 
-import java.net.URL;
+import java.io.File;
+import java.io.FileInputStream;
 
-import com.google.common.base.Charsets;
-import com.google.common.io.Resources;
+import org.apache.commons.io.IOUtils;
+
 import com.google.gson.Gson;
 
 public class TestUtility {
 	
-	private static final String DEFAULT_LOCATION = "/src/test/resources/";
-
-	@SuppressWarnings("restriction")
+	private static final String TEST_RESOURCE_PATH = System.getProperty("user.dir")+".src.test.resources.";
+	private static final String SEPERATOR = System.getProperty("file.separator");
+	
 	public static String loadFile(String fileName){
 		String fileContent = null;
 		try{
-			String packagePath = getCallerPackageName().replace(".", "/");
-			//String packagePath = sun.reflect.Reflection.getCallerClass(3).getPackage().getName().replace(".", "/");
-			System.out.println(packagePath);
-			String filePath = new StringBuffer().append(DEFAULT_LOCATION).append(packagePath).append("/").append(fileName).toString();
-			System.out.println(filePath);
-			URL url = Resources.getResource(filePath);
-			fileContent = Resources.toString(url, Charsets.UTF_8);
+			fileContent = IOUtils.toString(new FileInputStream(new File(getFilePath(fileName))));
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		return fileContent;
 	}
 	
-	public static String getCallerPackageName() { 
-	    StackTraceElement[] stElements = Thread.currentThread().getStackTrace();
-	    String callerPackageName = null;
-	    for (int i=1; i<stElements.length; i++) {
-	        StackTraceElement ste = stElements[i];
-	        if (!ste.getClassName().equals(TestUtility.class.getName()) && ste.getClass().getPackage().getName().indexOf("java.lang.Thread")!=0) {
-	            if (callerPackageName==null) {
-	            	callerPackageName = sun.reflect.Reflection.getCallerClass(i).getPackage().getName();
-	                //callerPackageName = ste.getClass().getPackage().getName();
-	            }
-	        }
-	    }
-	    return callerPackageName;
-	 }
-	
+	private static String getFilePath(String fileName) {
+		String folderPath = TEST_RESOURCE_PATH+getCallerClassPackageName()+".";
+		return folderPath.replace(".", SEPERATOR)+fileName;
+	}
+
+	private static String getCallerClassPackageName() {
+		StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+		for(StackTraceElement stackTraceElement: stackTraceElements){
+			String className = stackTraceElement.getClassName();
+			if(!className.equals(TestUtility.class.getName()) && className.indexOf("java.lang") == -1){
+				return className.substring(0, className.lastIndexOf("."));
+			}
+			
+		}
+		return null;
+	}
+
 	public static <T> T createObjectFromJsonFile(String fileName, Class<T> responseClass){
 		return new Gson().fromJson(loadFile(fileName), responseClass);
 	}
